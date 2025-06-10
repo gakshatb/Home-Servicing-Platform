e7b51e36e1f5500d8a6e912364729f17367d0fed
Akshat B. Gupta <129939442+gakshatb@users.noreply.github.com>
2025-06-10 16:23:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1218,5 +1218,4 @@ def add_admin():
 
 if __name__ == '__main__':
     add_admin()
-    port = int(os.environ.get('PORT', 4000))
-    app.run(host='0.0.0.0',port=port)
+    app.run(host='0.0.0.0',port=8000)
